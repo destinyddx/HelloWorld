478ec06a8ffcc2ef51d713ece0887a9a967c90bb
destinyddx <34993584+destinyddx@users.noreply.github.com>
2018-08-06 18:37:16 +0800

    Add files via upload

---
+++ preProcessing.py
@@ -0,0 +1,55 @@
+import numpy as np
+
+class StandardScaler:
+
+    def __init__(self):
+        self.mean_ = None
+        self.scale_ = None
+
+    def fit(self, X):
+        """根据训练数据集X获得数据的均值和方差"""
+        assert X.ndim == 2, "The dimmension of X must be 2"
+
+        self.mean_ = np.array([np.mean(X[:,i]) for i in range(X.shape[1])])
+        self.scale_ = np.array([np.std(X[:, i]) for i in range(X.shape[1])])
+
+        return self
+
+    def tranform(self, X):
+        """将X根据这个StandardScale进行均值方差归一化处理"""
+        assert X.ndim == 2, "The dimmension of X must be 2"
+        assert self.mean_ is not None and self.scale_ is not None, \
+            "must fit before transform"
+        assert X.shape[1] == len(self.mean_), \
+            " the fature of X must be equals to mean_ and std_"
+
+        resX = np.empty(shape=X.shape, dtype=float)
+        for col in range(X.shape[1]):
+            resX[:, col] = (X[:, col] - self.mean_[col]) / self.scale_[col]
+
+        return  resX
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
